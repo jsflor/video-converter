f7d566d7bbe3cf6ae2529a6c7e52fc33dbe7bc07
Sebastian Flor <sebastian.flor@atheneum.ai>
2024-01-24 16:56:17 +0100

    fix: errors

--- src/converter/consumer.py
+++ src/converter/consumer.py
@@ -11,13 +11,11 @@ def main():
     # gridfs
     fs_videos = gridfs.GridFS(db_videos)
     fs_mp3s = gridfs.GridFS(db_mp3s)
-    
+
     # rabbitmq connection
-    connection = pika.BlockingConnection(
-        pika.ConnectionParameters(host="rabbitmq")
-    )
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host="rabbitmq"))
     channel = connection.channel()
-    
+
     def callback(ch, method, properties, body):
         err = to_mp3.start(body, fs_videos, fs_mp3s, ch)
         if err:
@@ -26,12 +24,11 @@ def main():
             ch.basic_ack(delivery_tag=method.delivery_tag)
 
     channel.basic_consume(
-        queue=os.environ.get("VIDEO_QUEUE"),
-        on_message_callback=callback
+        queue=os.environ.get("VIDEO_QUEUE"), on_message_callback=callback
     )
-    
-    print("Waiting for messages. For exit press CTRL + C")
-    
+
+    print("Waiting for messages. To exit press CTRL+C")
+
     channel.start_consuming()
 
 if __name__ == "__main__":
--- src/gateway/storage/util.py
+++ src/gateway/storage/util.py
@@ -3,15 +3,16 @@ import pika, json
 def upload(f, fs, channel, access):
     try:
         fid = fs.put(f)
-    except:
+    except Exception as err:
+        print(err)
         return "internal server error", 500
-    
+
     message = {
         "video_fid": str(fid),
         "mp3_fid": None,
         "username": access["username"],
     }
-    
+
     try:
         channel.basic_publish(
             exchange="",
@@ -21,6 +22,7 @@ def upload(f, fs, channel, access):
                 delivery_mode=pika.spec.PERSISTENT_DELIVERY_MODE
             ),
         )
-    except:
+    except Exception as err:
+        print(err)
         fs.delete(fid)
         return "internal server error", 500
